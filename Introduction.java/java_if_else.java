d3329a70e0f9bbb50d2bc8f7e16ffedfe5aa047e
Biozid Bhuiuan Tonoy <tawsiftonoy007@gmail.com>
2023-11-16 19:52:15 +0600

    init

---
+++ Introduction.java/java_if_else.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class java_if_else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 2 == 0 && n <= 5) {
+            System.out.println("Not Weird");
+        } else if (n % 2 == 0 && n <= 20) {
+            System.out.println("Weird");
+        } else if (n % 2 == 0 && n > 20) {
+            System.out.println("Not Weird");
+        } else {
+            System.out.println("Weird");
+        }
+    }
+
+}
